87759fba3f3ef51bbba2003ec96137a659196f1b
ssusa2 <audgns9207@naver.com>
2022-07-15 18:17:41 +0900

    간단한 유저정보 기능 리팩토링

--- input.js
+++ input.js
@@ -1,47 +1,40 @@
 /** @format */
-const loginForm = document.querySelector('#login-form');
 
+const loginForm = document.querySelector('#login-form');
 const loginInput = loginForm.querySelector('input');
 const loginButton = loginForm.querySelector('button');
-
 const gretting = document.querySelector('#greeting');
 const link = document.querySelector('a');
 
 const HIDDEN_CLASSNAME = 'hidden';
-const hanbleClick = (e) => {
+const GrettingMent = gretting.innerText;
+const savedUsername = localStorage.getItem('username');
+const FORM_LIST = loginForm.classList;
+
+const handleClick = (e) => {
 	e.preventDefault();
 	const username = loginInput.value;
-	loginForm.classList.add('hidden');
-	console.log(username);
+	FORM_LIST.add('hidden');
 };
 
-link.addEventListener('click', hanbleClick);
+link.addEventListener('click', handleClick);
+
+function paintGreetings(username) {
+	gretting.classList.remove(HIDDEN_CLASSNAME);
+	gretting.innerText = `Hello ${username}`;
+}
 
 function onLoginsubmit(e) {
 	e.preventDefault();
-
-	loginForm.classList.add(HIDDEN_CLASSNAME);
+	FORM_LIST.add(HIDDEN_CLASSNAME);
 	const username = loginInput.value;
-	console.log(username);
-	gretting.innerText = `Hello ${username}`;
-	gretting.classList.remove(HIDDEN_CLASSNAME);
-
 	localStorage.setItem('username', username);
-	// if (value === '') {
-	// 	alert('이름을 입력하세요');
-	// } else if (value.length > 14) {
-	// 	alert('이름이 너무 길어요!');
-	// }
+	paintGreetings(username);
 }
-
-const savedUsername = localStorage.getItem('username');
 
 if (savedUsername === null) {
-	loginForm.classList.remove(HIDDEN_CLASSNAME);
-	loginForm.addEventListener('submit', onLoginsubmit);
+	FORM_LIST.remove(HIDDEN_CLASSNAME);
+	loginButton.addEventListener('click', onLoginsubmit);
 } else {
-	gretting.classList.remove(HIDDEN_CLASSNAME);
-	gretting.innerText = `Hello ${savedUsername}`;
+	paintGreetings(savedUsername);
 }
-
-loginButton.addEventListener('click', onclick);
